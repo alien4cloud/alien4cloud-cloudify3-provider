8ee9b5c961f78fec379442ad9d1cc7f73566f51e
igor Ngouagna <igor.ngouagna@fastconnect.fr>
2016-09-15 16:32:39 +0200

    fixed build

--- src/test/java/alien4cloud/paas/cloudify3/util/ApplicationUtil.java
+++ src/test/java/alien4cloud/paas/cloudify3/util/ApplicationUtil.java
@@ -3,23 +3,24 @@ package alien4cloud.paas.cloudify3.util;
 import alien4cloud.application.ApplicationService;
 import alien4cloud.dao.ElasticSearchDAO;
 import alien4cloud.model.application.Application;
-import org.alien4cloud.tosca.model.templates.Topology;
 import alien4cloud.paas.cloudify3.AbstractTest;
-import org.alien4cloud.tosca.catalog.ArchiveParser;
 import alien4cloud.tosca.model.ArchiveRoot;
 import alien4cloud.tosca.parser.ParsingException;
 import alien4cloud.tosca.parser.ParsingResult;
 import alien4cloud.utils.FileUtil;
 import alien4cloud.utils.MapUtil;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.alien4cloud.tosca.catalog.ArchiveParser;
+import org.alien4cloud.tosca.model.templates.Topology;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import javax.annotation.Resource;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.springframework.stereotype.Component;
 
 @Component("cfy3-applicationUtil")
 @Slf4j
@@ -44,21 +45,23 @@ public class ApplicationUtil {
         if (application != null) {
             applicationService.delete(application.getId());
         }
-        Topology topology = parseYamlTopology(topologyFileName, locationName);
+        ArchiveRoot archiveRoot = parseYamlTopology(topologyFileName, locationName);
         String applicationId = applicationService.create("alien", applicationName, null);
-        topology.setDelegateId(applicationId);
-        topology.setDelegateType(Application.class.getSimpleName().toLowerCase());
-        alienDAO.save(topology);
-        return topology;
+        // TODO validate this works
+        archiveRoot.getArchive().setDelegateId(applicationId);
+        archiveRoot.getArchive().setDelegateType(Application.class.getSimpleName().toLowerCase());
+        alienDAO.save(archiveRoot.getArchive());
+        alienDAO.save(archiveRoot.getTopology());
+        return archiveRoot.getTopology();
     }
 
-    private Topology parseYamlTopology(String topologyFileName, String locationName) throws IOException, ParsingException {
+    private ArchiveRoot parseYamlTopology(String topologyFileName, String locationName) throws IOException, ParsingException {
         Path zipPath = Files.createTempFile("csar", ".zip");
         Path realTopologyPath = Files.createTempFile(topologyFileName, ".yaml");
         VelocityUtil.generate(Paths.get("src/test/resources/topologies/" + locationName + "/" + topologyFileName + ".yaml"), realTopologyPath,
                 MapUtil.newHashMap(new String[] { "projectVersion" }, new String[] { AbstractTest.VERSION }));
         FileUtil.zip(realTopologyPath, zipPath);
         ParsingResult<ArchiveRoot> parsingResult = parser.parse(zipPath);
-        return parsingResult.getResult().getTopology();
+        return parsingResult.getResult();
     }
 }
