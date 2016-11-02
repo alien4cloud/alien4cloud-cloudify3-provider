630563cba9fee408abf844a30034e63ba7e51894
Victor Keophila <victor.keophila@fastconnect.fr>
2016-11-02 19:04:33 +0100

    Ensure that the computes its volumes are on the same availability zone

--- src/main/resources/recipe/plugin_overrides/amazon/ec2/wrapper.py
+++ src/main/resources/recipe/plugin_overrides/amazon/ec2/wrapper.py
@@ -3,7 +3,20 @@ from cloudify.decorators import operation
 
 from a4c_common.wrapper_util import (USE_EXTERNAL_RESOURCE_KEY,handle_external_resource,handle_resource_ids)
 
+from ec2.instance import run_instances
 from ec2.ebs import create
+
+
+@operation
+def overrided_run_instance(**_):
+  if 'parameters' in ctx.node.properties and 'placement' in ctx.node.properties['parameters']:
+    if ('scale' in ctx.workflow_id):
+      # Ignore the placement property when scale
+      ctx.instance.runtime_properties['placement'] = None
+    else:
+      ctx.instance.runtime_properties['placement'] = ctx.node.properties['parameters']['placement']
+  run_instances(**_)
+
 
 @operation
 def overrided_create_volume(args, **_):
