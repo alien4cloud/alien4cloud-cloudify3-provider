9ffb90a6de1f8b4c065f37fdbd74536bd07abda3
Victor Keophila <victor.keophila@fastconnect.fr>
2016-11-03 12:40:25 +0100

    Fix aws

--- src/main/resources/recipe/plugin_overrides/amazon/ec2/wrapper.py
+++ src/main/resources/recipe/plugin_overrides/amazon/ec2/wrapper.py
@@ -1,7 +1,7 @@
 from cloudify import ctx
 from cloudify.decorators import operation
 
-from a4c_common.wrapper_util import (USE_EXTERNAL_RESOURCE_KEY,handle_external_resource,handle_resource_ids)
+from a4c_common.wrapper_util import (A4C_RESOURCE_ID_KEY,USE_EXTERNAL_RESOURCE_KEY,handle_external_resource,handle_resource_ids)
 
 from ec2.instance import run_instances
 from ec2.ebs import create
@@ -20,6 +20,7 @@ def overrided_run_instance(**_):
 
 @operation
 def overrided_create_volume(args, **_):
+  ctx.instance.runtime_properties[ctx.node.properties[A4C_RESOURCE_ID_KEY]]= None
   handle_external_resource()
   if(ctx.instance.runtime_properties[USE_EXTERNAL_RESOURCE_KEY]):
     handle_resource_ids()
