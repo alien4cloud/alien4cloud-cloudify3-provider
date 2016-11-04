f97a17482f503551708980300e7d3c6a18f75318
Victor Keophila <victor.keophila@fastconnect.fr>
2016-11-04 19:38:33 +0100

    Fix issue to handle the placement properties when deploying several instances on aws

--- src/main/resources/recipe/plugin_overrides/a4c_common/wrapper_util.py
+++ src/main/resources/recipe/plugin_overrides/a4c_common/wrapper_util.py
@@ -38,11 +38,13 @@ def handle_external_resource():
 
 def handle_resource_ids():
   try:
-    lock = Lock("/tmp/lock_name_{}.tmp".format(ctx.deployment.id))
+    lock = Lock("/tmp/lock_name_{}_volume.tmp".format(ctx.deployment.id))
     lock.acquire()
 
+    volume_filepath = _get_volume_filepath()
+
     # Get the last instance resource index
-    instance_index = _get_index()
+    instance_index = _get_index(volume_filepath)
 
     # Retrieve the IaaS resource id key
     resource_id_key = ctx.node.properties[A4C_RESOURCE_ID_KEY]
@@ -58,7 +60,7 @@ def handle_resource_ids():
 
       ctx.logger.info("[A4C_VOLUME] Set existing resource (id={}) for volume (id={})".format(ctx.instance.runtime_properties[resource_id_key], ctx.instance.id))
       # Increment the instance index
-      _write_index(instance_index + 1)
+      _write_index(volume_filepath, (instance_index + 1))
     else:
       ctx.logger.warning("[A4C_VOLUME] Create new volume for instance {} as there isn't enough existing volume ids on the list {}".format(ctx.instance.id, resource_ids))
       ctx.instance.runtime_properties[resource_id_key] = None
@@ -66,18 +68,18 @@ def handle_resource_ids():
   finally:
     lock.release()
 
-def _get_index():
-  if(os.path.isfile(_get_filepath())):
-    with open(_get_filepath(), 'r') as f:
+def _get_index(filepath):
+  if(os.path.isfile(filepath)):
+    with open(filepath, 'r') as f:
       line = f.read()
       if(line):
         return int(line)
   return 0
 
-def _write_index(index):
-  ctx.logger.debug("[A4C_VOLUME] writing {} into {}".format(index, _get_filepath()))
-  with open(_get_filepath(), 'w') as f:
+def _write_index(filepath, index):
+  ctx.logger.debug("[A4C_VOLUME] writing {} into {}".format(index, filepath))
+  with open(filepath, 'w') as f:
     f.write(str(index))
 
-def _get_filepath():
+def _get_volume_filepath():
   return "./deployments/{}/volume_index_{}.dat".format(ctx.deployment.id, ctx.node.id)
--- src/main/resources/recipe/plugin_overrides/amazon/ec2/wrapper.py
+++ src/main/resources/recipe/plugin_overrides/amazon/ec2/wrapper.py
@@ -1,11 +1,12 @@
 from cloudify import ctx
 from cloudify.decorators import operation
 
-from a4c_common.wrapper_util import (A4C_RESOURCE_ID_KEY,USE_EXTERNAL_RESOURCE_KEY,handle_external_resource,handle_resource_ids)
+from a4c_common.wrapper_util import (A4C_RESOURCE_ID_KEY,USE_EXTERNAL_RESOURCE_KEY,handle_external_resource,Lock,_get_index,_write_index)
 
 from ec2.instance import run_instances
 from ec2.ebs import create
 
+A4C_INSTANCE_INDEX = "_a4c_instance_index"
 
 @operation
 def overrided_run_instance(**_):
@@ -14,14 +15,72 @@ def overrided_run_instance(**_):
       # Ignore the placement property when scale
       ctx.instance.runtime_properties['placement'] = None
     else:
-      ctx.instance.runtime_properties['placement'] = ctx.node.properties['parameters']['placement']
+      _set_placement_for_instance()
   run_instances(**_)
 
+def _set_placement_for_instance():
+  try:
+    lock = Lock("/tmp/lock_name_{}_placement.tmp".format(ctx.deployment.id))
+    lock.acquire()
+
+    placement_filepath = _get_placement_filepath()
+
+    # Get the last instance resource index
+    instance_index = _get_index(placement_filepath)
+
+    # Retrieve the IaaS resource id values (list of ids seperated by comma)
+    placements = ctx.node.properties['parameters']['placement']
+
+    placement_array = placements.split(",")
+
+    if(instance_index < len(placement_array) ):
+      # Save the resource id in the runtime properties for the instance
+      ctx.instance.runtime_properties['placement'] = placement_array[instance_index]
+      ctx.instance.runtime_properties[A4C_INSTANCE_INDEX]=instance_index
+
+      ctx.logger.info("[A4C_PLACEMENT] Set placement '{}'' for compute id={} index={}".format(ctx.instance.runtime_properties['placement'], ctx.instance.id, instance_index))
+      # Increment the instance index
+      _write_index(placement_filepath, (instance_index + 1))
+    else:
+      ctx.logger.warning("[A4C_PLACEMENT] No placement for instance {} as there is no more available zones on the list {}".format(ctx.instance.id, placements))
+      ctx.instance.runtime_properties['placement'] = None
+  finally:
+    lock.release()
+
+def _get_placement_filepath():
+  return "./deployments/{}/placement_index_{}.dat".format(ctx.deployment.id, ctx.node.id)
 
 @operation
 def overrided_create_volume(args, **_):
   ctx.instance.runtime_properties[ctx.node.properties[A4C_RESOURCE_ID_KEY]]= None
   handle_external_resource()
   if(ctx.instance.runtime_properties[USE_EXTERNAL_RESOURCE_KEY]):
-    handle_resource_ids()
+    handle_aws_resource_ids()
   create(args, **_)
+
+
+def handle_aws_resource_ids():
+
+  compute_id = ctx.instance.relationships[0].target.instance.id
+
+  instance_index = -1
+  if( A4C_INSTANCE_INDEX in ctx.instance.relationships[0].target.instance.runtime_properties ):
+    # Retrieve the index instance
+    instance_index = ctx.instance.relationships[0].target.instance.runtime_properties[A4C_INSTANCE_INDEX]
+
+  # Retrieve the IaaS resource id key
+  resource_id_key = ctx.node.properties[A4C_RESOURCE_ID_KEY]
+
+  # Retrieve the IaaS resource id values (list of ids seperated by comma)
+  resource_ids = ctx.node.properties[resource_id_key]
+
+  ids_array = resource_ids.split(",")
+
+  if( instance_index != -1 and instance_index < len(ids_array) ):
+    # Save the resource id in the runtime properties for the instance
+    ctx.instance.runtime_properties[resource_id_key] = ids_array[instance_index]
+    ctx.logger.info("[A4C_VOLUME] Set existing resource (id={}) for volume (id={}) linked to compute (id={})".format(ctx.instance.runtime_properties[resource_id_key], ctx.instance.id, compute_id))
+  else:
+    ctx.logger.warning("[A4C_VOLUME] Create new volume for instance {} as there no match index to get a volume id on the list {} (index={})".format(ctx.instance.id, resource_ids, instance_index))
+    ctx.instance.runtime_properties[resource_id_key] = None
+    ctx.instance.runtime_properties[USE_EXTERNAL_RESOURCE_KEY] = False 
