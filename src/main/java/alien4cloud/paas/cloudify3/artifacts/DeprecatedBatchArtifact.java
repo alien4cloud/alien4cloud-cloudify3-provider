3534e31cde1120dd7a5a0e317ef65ca6ba1ad771
Xavier DEGENNE <xavier@fastconnect.fr>
2017-04-20 16:28:01 +0200

    Support both old and renamed batch artifacts

--- src/main/java/alien4cloud/paas/cloudify3/artifacts/DeprecatedBatchArtifact.java
+++ src/main/java/alien4cloud/paas/cloudify3/artifacts/DeprecatedBatchArtifact.java
@@ -1,19 +1,17 @@
 package alien4cloud.paas.cloudify3.artifacts;
 
-import alien4cloud.paas.cloudify3.configuration.CloudConfiguration;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 /**
  * Windows batch artifact support. Note that it actually uses the same python script wrapper as the bash artifact.
  */
 @Component
-public class BatchArtifact extends AbstractBatchArtifact {
+@Deprecated
+public class DeprecatedBatchArtifact extends AbstractBatchArtifact {
 
     @Override
     public String getArtifactName() {
-        return "org.alien4cloud.artifacts.BatchScript";
+        return "alien.artifacts.BatchScript";
     }
 
 }
