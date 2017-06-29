048269dfc842652cfb33e293d016c974868eb7dd
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2017-06-29 16:06:08 +0200

    fixed Deployment update client by adding manager url to request

--- src/main/java/alien4cloud/paas/cloudify3/restclient/DeploymentUpdateClient.java
+++ src/main/java/alien4cloud/paas/cloudify3/restclient/DeploymentUpdateClient.java
@@ -1,11 +1,12 @@
 package alien4cloud.paas.cloudify3.restclient;
 
-import alien4cloud.paas.cloudify3.model.Blueprint;
-import alien4cloud.paas.cloudify3.util.FutureUtil;
-import alien4cloud.utils.FileUtil;
-import com.google.common.util.concurrent.ListenableFuture;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.inject.Inject;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -14,10 +15,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import alien4cloud.paas.cloudify3.configuration.CloudConfigurationHolder;
+import alien4cloud.paas.cloudify3.util.FutureUtil;
+import alien4cloud.utils.FileUtil;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by xdegenne on 06/03/2017.
@@ -28,6 +33,10 @@ public class DeploymentUpdateClient extends AbstractClient {
 
     // FIXME: actually we can not query anything but backend REST API
     public static final String DEPLOYMENT_UPDATE_PATH = "/backend/cloudify-api/deployment-updates";
+
+    @Inject
+    @Setter
+    private CloudConfigurationHolder configurationHolder;
 
     @Override
     protected String getPath() {
@@ -50,11 +59,15 @@ public class DeploymentUpdateClient extends AbstractClient {
         try {
             MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
             headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
-            return FutureUtil.unwrapRestResponse(exchange(getSuffixedUrl("/{id}/update/initiate", "application_file_name"), HttpMethod.POST,
+            return FutureUtil.unwrapRestResponse(exchange(getSuffixedUrl(getManagerUrl(), "/{id}/update/initiate", "application_file_name"), HttpMethod.POST,
                     new HttpEntity<>(Files.readAllBytes(destination.toPath()), headers), Void.class, deploymentId, sourceName));
         } finally {
             destination.delete();
         }
+    }
+
+    private String getManagerUrl() {
+        return configurationHolder.getConfiguration().getUrl();
     }
 
     @SneakyThrows
