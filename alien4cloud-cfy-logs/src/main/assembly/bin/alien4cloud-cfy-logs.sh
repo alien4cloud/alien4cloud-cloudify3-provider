a61409f02e7f7aefce8bbf11f98e05decf5495e1
Luc Boutier <luc@fastconnect.fr>
2017-09-26 07:52:40 +0200

    Official package should not try to install java from a fastconnect url

--- alien4cloud-cfy-logs/src/main/assembly/bin/alien4cloud-cfy-logs.sh
+++ alien4cloud-cfy-logs/src/main/assembly/bin/alien4cloud-cfy-logs.sh
@@ -11,23 +11,7 @@ elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
     echo "found java executable in ${JAVA_HOME}"
     _java="$JAVA_HOME/bin/java"
 elif [ ! -d "$workdir/java" ]; then
-    echo "Installation JAVA"
-
-    JAVA_URL="https://fastconnect.org/owncloud/public.php?service=files&t=121703a2a53853b0ca32bfbc903c6208&download"
-
-    mkdir $workdir/java
-    cd $workdir/java
-
-    echo "Downloading jdk"
-    curl --insecure --silent -o $workdir/java.tar.gz -O $JAVA_URL
-
-    echo "Extracting java"
-    tar xfz $workdir/java.tar.gz -C $workdir/java
-    mv $workdir/java/*/* $workdir/java
-    echo "Using java executable in ${workdir}/java/bin"
-
-    _java=$workdir/java/bin/java
-    rm -f $workdir/java.tar.gz
+    echo "JAVA is required for the log application to run"
 fi
 
 
