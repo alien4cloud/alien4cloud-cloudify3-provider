3d5b778b9985983e288d9254bfccfb619178b9a8
Xavier DEGENNE <xavier@fastconnect.fr>
2018-06-07 05:02:16 +0200

    [ALIEN-2904] Fix : the GET url on Cfy backend executions endpoint was not correctly build so we was retrieving too much data and still have a partial snapshot

--- src/main/java/alien4cloud/paas/cloudify3/restclient/ExecutionBackendClient.java
+++ src/main/java/alien4cloud/paas/cloudify3/restclient/ExecutionBackendClient.java
@@ -5,6 +5,7 @@ import alien4cloud.paas.cloudify3.model.Execution;
 import alien4cloud.paas.cloudify3.model.GetBackendExecutionsResult;
 import alien4cloud.paas.cloudify3.util.FutureUtil;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multiset;
 import com.google.common.util.concurrent.ListenableFuture;
 import lombok.Setter;
 import lombok.SneakyThrows;
@@ -41,16 +42,42 @@ public class ExecutionBackendClient extends AbstractClient {
         if (log.isDebugEnabled()) {
             log.debug("List executions for deployments {}", deploymentIds);
         }
-        Map<String, Object> request = Maps.newHashMap();
-        request.put("deployment_id", deploymentIds);
-        request.put("_include", new String[]{"id", "workflow_id", "status", "deployment_id"});
+        Map<String, String[]> parameters = Maps.newHashMapWithExpectedSize(deploymentIds.length + 1);
+        Map<String, String> parameterValues = Maps.newHashMapWithExpectedSize(deploymentIds.length + 1);
+        parameters.put("_include", new String[]{"id,workflow_id,status,deployment_id"});
+        parameters.put("deployment_id", deploymentIds);
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
         headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
 
-        return FutureUtil.unwrapRestResponse(getForEntity(getBaseUrl(getManagerUrl()), GetBackendExecutionsResult.class, request));
+        return FutureUtil.unwrapRestResponse(getForEntity(getFullUrl(getManagerUrl(), parameters), GetBackendExecutionsResult.class));
     }
 
     private String getManagerUrl() {
         return configurationHolder.getConfiguration().getUrl();
     }
+
+    /**
+     * Get the url with array parameters in the form param=v1@param=v2&param=v3
+     * We don't want this query to cached by RestTemplate ...
+     */
+    public String getFullUrl(String managerUrl, Map<String, String[]> parameters) {
+        String urlPrefix = managerUrl + getPath();
+        if (parameters != null && parameters.size() > 0) {
+            StringBuilder urlBuilder = new StringBuilder(urlPrefix);
+            if (!parameters.isEmpty()) {
+                urlBuilder.append("?");
+                for (Map.Entry<String, String[]> parameter : parameters.entrySet()) {
+                    for (String parameterValue : parameter.getValue()) {
+                        urlBuilder.append(parameter.getKey()).append("=").append(parameterValue).append("&");
+                    }
+                }
+                // remove the last & (or ? if all parameters have 0 value)
+                urlBuilder.setLength(urlBuilder.length() - 1);
+            }
+            return urlBuilder.toString();
+        } else {
+            return urlPrefix;
+        }
+    }
+
 }
